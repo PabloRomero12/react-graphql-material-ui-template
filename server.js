9fdefd723450415edc5baf1c6dbff9da70a2f127
pabloRS12 <pablo_rs12@yahoo.com>
2019-05-17 19:24:44 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "variables.env" });
+
+const Recipe = require("./models/Recipe");
+const User = require("./models/User");
+
+// Bring graphql
+const graphqlHTTP = require("express-graphql");
+const { makeExecutableSchema } = require("graphql-tools");
+
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+// Connects to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongo connected");
+  })
+  .catch(err => console.log(err));
+
+// Initialize application
+
+const app = express();
+
+// Connect schemas to GraphQL
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+    context: {
+      Recipe,
+      User
+    }
+  })
+);
+/*
+app.use(
+  "graphql",
+  graphqlExpress({
+    schema,
+    context: {
+      Recipe,
+      User
+    }
+  })
+);
+*/
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
